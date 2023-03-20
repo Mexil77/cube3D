4a9b38ea1df77df2f8ef36127ccff0927b1d3acd
Emmanuel Garcia torres <emgarcia@c3r1s2.42madrid.com>
2023-03-20 18:47:01 +0100

    try clean

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vguttenb <vguttenb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 12:54:27 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/08/19 18:19:03 by vguttenb         ###   ########.fr       */
+/*   Updated: 2023/03/20 18:20:51 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,31 +33,20 @@ int	ft_keyhook(int keycode, t_general *g)
 	return (0);
 }
 
-void	ft_leaks(void)
-{
-	system("leaks cube3d");
-}
-
 void	init_struct(t_general *g)
 {
-	// g->window_width = g->map_width * TILE_SIZE;
-	// g->window_height = g->map_height * TILE_SIZE;
 	g->game = true;
-	g->frame = 0; //DUDA: POR QUÉ ESTO? YA ES CERO
+	g->frame = 0;
 	g->ang = 0;
 	g->move_speed = 4;
 	g->rotate_speed = 4;
-
-	//SOBRESCRITURA DE LA POSICIÓN DEL JUGADOR PARA PRUEBAS DE RAYCAST
 	g->posx = 1535.484375;
 	g->posy = 384.378906;
 	g->ang = 320;
-	
 }
 
 int	ft_keypress(int keycode, t_general *g)
 {
-	//printf("key code is %d\n", keycode);
 	if (keycode == 53)
 		close_window(g);
 	if (keycode == 13)
@@ -68,7 +57,7 @@ int	ft_keypress(int keycode, t_general *g)
 		g->move_dir = -1;
 	else if (keycode == 2)
 		g->rotate_dir = 1;
-	else if (keycode == 37) // L
+	else if (keycode == 37)
 		draw_pov_diagn(g);
 	return (0);
 }
@@ -95,16 +84,13 @@ int	main(int argc, char **argv)
 		printf("bad arguments.\n");
 		return (0);
 	}
-	// atexit(ft_leaks);
-	general = (t_general *)ft_calloc(sizeof(t_general), 1); // DUDA: POR QUÉ ALOCAR NUESTRA VARIABLE GENERAL
+	general = (t_general *)ft_calloc(sizeof(t_general), 1);
 	printf("mapa : %s\n", argv[1]);
-	parse_map(general, argv[1]); // RENOMBRADO
+	parse_map(general, argv[1]);
 	init_struct(general);
 	init_window(general);
-	// ft_printgeneral(general);
 	mlx_hook(general->win, 2, (1L << 0), ft_keypress, general);
 	mlx_hook(general->win, 3, (1L << 1), ft_keyrelease, general);
-	// mlx_key_hook(general->win, ft_keyhook, general);
 	mlx_loop_hook(general->mlx, game_loop, general);
 	mlx_loop(general->mlx);
 	free_all(general);
