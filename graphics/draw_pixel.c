8e6c4e521d2b93c6217bd1aabd6fb15081319059
Emmanuel Garcia torres <emgarcia@c3r1s2.42madrid.com>
2023-03-20 19:04:07 +0100

    cleaning

--- graphics/draw_pixel.c
+++ graphics/draw_pixel.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_pixel.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vguttenb <vguttenb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 20:05:19 by vguttenb          #+#    #+#             */
-/*   Updated: 2022/11/29 20:01:24 by vguttenb         ###   ########.fr       */
+/*   Updated: 2023/03/20 19:02:04 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ void	draw_pixel(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	// if (y < 0 || y >= img->img_height || x < 0 || x >= img->img_width)
-	// {
-	// 	printf("CUB3D ERROR: tried painting pixel from outside image dimensions\n\ttried painting pixel in x: %d and y: %d\n\tfor an image of dimensions x: %d and y: %d\n", x, y, img->img_width, img->img_height);
-	// 	exit(1);
-	// }
 	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
